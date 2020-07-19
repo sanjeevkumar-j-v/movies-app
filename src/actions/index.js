ebfbeced7682f96435ddd91635733c0d1e326927
sanjeev kumar <sanjeevkumarjv@gmail.com>
2020-07-19 08:33:53 +0530

    Add action and reducer

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+// {
+//     type: 'ADD_MOVIES'   
+// },
+// {
+//     type: 'REMOVE_MOVIES'
+// }
--- src/component/App.js
+++ src/component/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import { data } from '../data';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
 function App() {
   return (
-    <div >
-      Movie App
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favorite</div>
+        </div>
+
+        <div className="list">
+          {data.map(movie => (
+            <MovieCard movie={movie}/>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
